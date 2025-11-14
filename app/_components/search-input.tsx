8967e6b0ca5d226d6570cc9b91cb44585546e41b
Anderson Riquelmer Modesto Santos <82468314+riquelmer45@users.noreply.github.com>
2025-11-14 16:09:21 -0300

    feat: add booking item component

--- app/_components/search-input.tsx
+++ app/_components/search-input.tsx
@@ -4,11 +4,11 @@ import { Input } from "./ui/input";
 
 const SearchInput = () => {
   return (
-    <div className="flex  items-center gap-2">
+    <div className="flex items-center gap-2">
       <Input
         type="text"
         placeholder="Pesquise serviços ou barbearias"
-        className="rounded-full border-border"
+        className="border-border rounded-full"
       />
       <Button variant="default" size="icon" className="rounded-full">
         <SearchIcon />
--- app/page.tsx
+++ app/page.tsx
@@ -2,21 +2,31 @@ import Image from "next/image";
 import Header from "./_components/header";
 import SearchInput from "./_components/search-input";
 import banner from "../public/banner.png";
+import BookingItem from "./_components/booking-item";
 
 const home = () => {
   return (
-    <div>
+    <main>
       <Header />
-      <div className="px-5 space-y-4">
+      <div className="space-y-4 px-5">
         <SearchInput />
         <Image
           src={banner}
           alt="Banner"
           sizes="100vw"
-          className="w-full h-auto"
+          className="h-auto w-full"
+        />
+        <h2 className="text-foreground text-xs font-semibold uppercase">
+          Agendamentos
+        </h2>
+        <BookingItem
+          serviceName="Corte de Cabelo"
+          barbershopName="Barbearia do João"
+          barbershopImageUrl="https://utfs.io/f/c97a2dc9-cf62-468b-a851-bfd2bdde775f-16p.png"
+          date={new Date()}
         />
       </div>
-    </div>
+    </main>
   );
 };
 export default home;
